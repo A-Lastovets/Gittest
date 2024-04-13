8f0ead6f1a6ce4fbeb9159c4088a666be4fbb260
A-Lastovets <164244571+A-Lastovets@users.noreply.github.com>
2024-04-13 23:37:00 +0200

    trainee

--- file.py
+++ file.py
@@ -1,9 +1,12 @@
-n = int(input('Enter number of last range: '))
-s = 0
-i = 1
-while i <= n:
-    s = i + s
-    i += 1
+# n = int(input('Enter number of last range: '))
+# s = 0
+# i = 1
+# while i <= n:
+#     s = i + s
+#     i += 1
 
-print(f'Sum below numbers 1 to {n}: ')
+# print(f'Sum below numbers 1 to {n}: ')
 
+my_list = [1, 2, 3, 4, 5]
+for i in range(len(my_list)):
+    print(f"Element {my_list[i]} got index {i}")
